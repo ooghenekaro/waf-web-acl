c1c77d62230aabeccf30d20e43e70b9f72bc0725
ooghenekaro <ooghenekaro@yahoo.com>
2024-01-31 09:20:02 +0100

    initial commit

---
+++ waf.tf
@@ -0,0 +1,386 @@
+module "label" {
+  source = "cloudposse/label/null"
+  # Cloud Posse recommends pinning every module to a specific version
+    version = "0.25.0"
+
+  namespace = "eg"
+  stage     = "prod"
+  name      = "waf"
+  delimiter = "-"
+
+  tags = {
+    "BusinessUnit" = "XYZ",
+  }
+}
+
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+module "waf" {
+    source = "cloudposse/waf/aws"
+
+    visibility_config = {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "rules-example-metric"
+    sampled_requests_enabled   = false
+  }
+
+  # https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-list.html
+  managed_rule_group_statement_rules = [
+    {
+      name     = "AWS-AWSManagedRulesAdminProtectionRuleSet"
+      priority = 1
+
+      statement = {
+        name        = "AWSManagedRulesAdminProtectionRuleSet"
+        vendor_name = "AWS"
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = true
+        sampled_requests_enabled   = true
+        metric_name                = "AWS-AWSManagedRulesAdminProtectionRuleSet"
+      }
+    },
+    {
+      name     = "AWS-AWSManagedRulesAmazonIpReputationList"
+      priority = 2
+
+      statement = {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = true
+        sampled_requests_enabled   = true
+        metric_name                = "AWS-AWSManagedRulesAmazonIpReputationList"
+      }
+    },
+    {
+      name     = "AWS-AWSManagedRulesCommonRuleSet"
+      priority = 3
+
+      statement = {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = true
+        sampled_requests_enabled   = true
+        metric_name                = "AWS-AWSManagedRulesCommonRuleSet"
+      }
+    },
+    {
+      name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+      priority = 4
+
+      statement = {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = true
+        sampled_requests_enabled   = true
+        metric_name                = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+      }
+    },
+    # https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-bot.html
+    {
+      name     = "AWS-AWSManagedRulesBotControlRuleSet"
+      priority = 5
+
+      statement = {
+        name        = "AWSManagedRulesBotControlRuleSet"
+        vendor_name = "AWS"
+
+        rule_action_override = {
+          CategoryHttpLibrary = {
+            action = "block"
+            custom_response = {
+              response_code = "404"
+              response_header = {
+                name  = "example-1"
+                value = "example-1"
+              }
+            }
+          }
+          SignalNonBrowserUserAgent = {
+            action = "count"
+            custom_request_handling = {
+              insert_header = {
+                name  = "example-2"
+                value = "example-2"
+              }
+            }
+          }
+        }
+
+        managed_rule_group_configs = [
+          {
+            aws_managed_rules_bot_control_rule_set = {
+              inspection_level = "COMMON"
+            }
+          }
+        ]
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = true
+        sampled_requests_enabled   = true
+        metric_name                = "AWS-AWSManagedRulesBotControlRuleSet"
+      }
+    }
+  ]
+
+  byte_match_statement_rules = [
+    {
+      name     = "rule-30"
+      action   = "allow"
+      priority = 30
+
+      statement = {
+        positional_constraint = "EXACTLY"
+        search_string         = "/cp-key"
+
+        text_transformation = [
+          {
+            priority = 30
+            type     = "COMPRESS_WHITE_SPACE"
+          }
+        ]
+
+        field_to_match = {
+          uri_path = {}
+        }
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+        metric_name                = "rule-30-metric"
+      }
+    }
+  ]
+
+  rate_based_statement_rules = [
+    {
+      name     = "rule-40"
+      action   = "block"
+      priority = 40
+
+      statement = {
+        limit              = 100
+        aggregate_key_type = "IP"
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+        metric_name                = "rule-40-metric"
+      }
+    }
+  ]
+
+  size_constraint_statement_rules = [
+    {
+      name     = "rule-50"
+      action   = "block"
+      priority = 50
+
+      statement = {
+        comparison_operator = "GT"
+        size                = 15
+
+        field_to_match = {
+          all_query_arguments = {}
+        }
+
+        text_transformation = [
+          {
+            type     = "COMPRESS_WHITE_SPACE"
+            priority = 1
+          }
+        ]
+
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+        metric_name                = "rule-50-metric"
+      }
+    }
+  ]
+
+  xss_match_statement_rules = [
+    {
+      name     = "rule-60"
+      action   = "block"
+      priority = 60
+
+      statement = {
+        field_to_match = {
+          uri_path = {}
+        }
+
+        text_transformation = [
+          {
+            type     = "URL_DECODE"
+            priority = 1
+          },
+          {
+            type     = "HTML_ENTITY_DECODE"
+            priority = 2
+          }
+        ]
+
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+        metric_name                = "rule-60-metric"
+      }
+    }
+  ]
+
+  sqli_match_statement_rules = [
+    {
+      name     = "rule-70"
+      action   = "block"
+      priority = 70
+
+      statement = {
+
+        field_to_match = {
+          query_string = {}
+        }
+
+        text_transformation = [
+          {
+            type     = "URL_DECODE"
+            priority = 1
+          },
+          {
+            type     = "HTML_ENTITY_DECODE"
+            priority = 2
+          }
+        ]
+
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+        metric_name                = "rule-70-metric"
+      }
+    }
+  ]
+
+  geo_match_statement_rules = [
+    {
+      name     = "rule-80"
+      action   = "count"
+      priority = 80
+
+      statement = {
+        country_codes = ["NL", "GB"]
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+        metric_name                = "rule-80-metric"
+      }
+    },
+    {
+      name     = "rule-11"
+      action   = "allow"
+      priority = 11
+
+      statement = {
+        country_codes = ["US"]
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+        metric_name                = "rule-11-metric"
+      }
+    }
+  ]
+
+  geo_allowlist_statement_rules = [
+    {
+      name     = "rule-90"
+      priority = 90
+
+      statement = {
+        country_codes = ["US"]
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+        metric_name                = "rule-90-metric"
+      }
+    }
+  ]
+
+  regex_match_statement_rules = [
+    {
+      name     = "rule-100"
+      priority = 100
+      action   = "block"
+
+      statement = {
+        regex_string = "^/admin"
+
+        text_transformation = [
+          {
+            priority = 90
+            type     = "COMPRESS_WHITE_SPACE"
+          }
+        ]
+
+        field_to_match = {
+          uri_path = {}
+        }
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+        metric_name                = "rule-100-metric"
+      }
+    }
+  ]
+
+  ip_set_reference_statement_rules = [
+    {
+      name     = "rule-110"
+      priority = 110
+      action   = "block"
+
+      statement = {
+        ip_set = {
+          ip_address_version = "IPV4"
+          addresses          = ["17.0.0.0/8"]
+        }
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+        metric_name                = "rule-110-metric"
+      }
+    }
+  ]
+
+  context = module.label.context
+}
